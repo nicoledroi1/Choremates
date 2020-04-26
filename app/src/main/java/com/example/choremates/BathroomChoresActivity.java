ed0745b630f485a2d976cb30c17bce1f4d1700a6
Nicole Droi <nicoled0210@hotmail.com>
2020-04-26 14:17:53 -0400

    living room chores page

---
+++ app/src/main/java/com/example/choremates/BathroomChoresActivity.java
@@ -0,0 +1,12 @@
+package com.example.choremates;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BathroomChoresActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living_room_chore_page);
+    }
+}
---
+++ app/src/main/java/com/example/choremates/BedroomChoresActivity.java
@@ -0,0 +1,12 @@
+package com.example.choremates;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BedroomChoresActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living_room_chore_page);
+    }
+}
