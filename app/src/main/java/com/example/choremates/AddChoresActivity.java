edcc425d905d3dd1c9179b877e32b784c32fa117
nive02 <nive0204@gmail.com>
2020-04-28 19:08:16 -0400

    DatabaseHelper

--- app/src/main/java/com/example/choremates/AddChoresActivity.java
+++ app/src/main/java/com/example/choremates/AddChoresActivity.java
@@ -1,6 +1,5 @@
 package com.example.choremates;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,13 +8,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class AddChoresActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AddChoresActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private Button kitchen;
     private Button bedroom;
@@ -27,6 +32,11 @@ public class AddChoresActivity extends AppCompatActivity {
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
     public Toolbar toolbar;
+
+    ExpandableListAdapter adapter;
+    ExpandableListView expandableListView;
+    List<MenuModel> headerList  = new ArrayList<>(); //list of all the headers
+    HashMap<MenuModel, List<MenuModel>> childList = new HashMap<>(); //hashmap of all the headers and their children
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,13 +97,25 @@ public class AddChoresActivity extends AppCompatActivity {
 //            }
 //        });
 
-        navigation();//don't forget to call it.
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        expandableListView = findViewById(R.id.expandableListView);
+
+        prepareMenuData();
+        populateExpandableList();
+
+        drawer = findViewById(R.id.chores_drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.red));
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    /*
-    Nicole you need to copy paste these next two methods into all ur activities that need the navigation
-    drawer.
-     */
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)){
@@ -103,36 +125,122 @@ public class AddChoresActivity extends AppCompatActivity {
         }
     }//end onBackPressed
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
-    private void navigation(){
-        drawer = findViewById(R.id.chores_drawer_layout);
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    private void prepareMenuData() {
+
+        MenuModel profileM = new MenuModel("Profile", true, false); //Menu of Android Tutorial. No sub menus
+        headerList.add(profileM);
+
+        if (!profileM.hasChildren) {
+            childList.put(profileM, null);
+        }
+
+        MenuModel myChoresM = new MenuModel("My Chores", true, false);
+        headerList.add(myChoresM);
+
+        if (!myChoresM.hasChildren) {
+            childList.put(myChoresM, null);
+        }
+
+        MenuModel addChoresM = new MenuModel("Add Chores", true, true);
+        headerList.add(addChoresM);
+        List<MenuModel> childModelsList = new ArrayList<>();
+        MenuModel kitchen = new MenuModel("Kitchen Chores", false, false);
+        childModelsList.add(kitchen);
+
+        MenuModel bathroom = new MenuModel("Bathroom Chores", false, false);
+        childModelsList.add(bathroom);
+
+        MenuModel livingRoom = new MenuModel("Living room Chores", false, false);
+        childModelsList.add(livingRoom);
+
+        MenuModel general = new MenuModel("General Chores", false, false);
+        childModelsList.add(general);
+
+        MenuModel bedroom = new MenuModel("Bedroom Chores", false, false);
+        childModelsList.add(bedroom);
+
+        MenuModel custom = new MenuModel("Custom Chores", false, false);
+        childModelsList.add(custom);
+
+        if (addChoresM.hasChildren) {
+            childList.put(addChoresM, childModelsList);
+        }
+
+        MenuModel calendarM = new MenuModel("Calendar", true, false); //Menu of Python Tutorials
+        headerList.add(calendarM);
+
+        if (!calendarM.hasChildren) {
+            childList.put(calendarM, null);
+        }
+    }
+
+    private void populateExpandableList() {
+
+        adapter = new ExpandableListAdapter(this, headerList, childList);
+        expandableListView.setAdapter(adapter);
+
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setCheckable(false);
-                switch (item.getItemId()){
-                    case R.id.nav_my_chores:
-                        startActivity(new Intent (getApplicationContext(), ChoresActivity.class));
-                        break;
-                    case R.id.nav_add_chores:
-                        startActivity(new Intent (getApplicationContext(), AddChoresActivity.class));
-                        break;
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                String gp = Integer.toString(groupPosition);
+                if (headerList.get(groupPosition).isGroup) {
+                    if (!headerList.get(groupPosition).hasChildren) {
+                        if (gp.equals("0")){
+                            //go to profile page
+                        } else if (gp.equals("1")){
+                            startActivity(new Intent (getApplicationContext(), ChoresActivity.class));
+                        } else if (gp.equals("3")){
+                            //go to calendar page
+                        }
+                    }
                 }
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
+
+                return false;
             }
         });
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                String cp = Integer.toString(childPosition);
+                if (childList.get(headerList.get(groupPosition)) != null) {
+                    switch (cp){
+                        case "0":
+                            startActivity(new Intent (getApplicationContext(), KitchenChoresActivity.class));
+                            break;
+                        case "1":
+                            startActivity(new Intent (getApplicationContext(), BedroomChoresActivity.class));
+                            break;
+                        case "2":
+                            startActivity(new Intent (getApplicationContext(), BathroomChoresActivity.class));
+                            break;
+                        case "3":
+                        case "4":
+                        case "5":
+                    }
+                }
 
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.red));
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-    }
+                return false;
+            }
+        });
+    }//end method
+
 }
