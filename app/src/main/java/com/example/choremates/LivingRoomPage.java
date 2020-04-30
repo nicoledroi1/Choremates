1b2507f8ae9931ab6b5fe32c3f240f4c17fa3468
Nicole Droi <nicoled0210@hotmail.com>
2020-04-30 15:59:06 -0400

    Commit living room

---
+++ app/src/main/java/com/example/choremates/LivingRoomPage.java
@@ -0,0 +1,14 @@
+package com.example.choremates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivingRoomPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_living_room_page);
+    }
+}
