cd878ba4497cfdd674ce7986d0585db378b9904f
nive02 <nive0204@gmail.com>
2020-04-29 22:53:05 -0400

    DatabaseHelper commit 2

--- app/src/main/java/com/example/choremates/DatabaseHelper.java
+++ app/src/main/java/com/example/choremates/DatabaseHelper.java
@@ -9,29 +9,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    /**
-     * constructor method
-     * @param context
-     */
+
     public DatabaseHelper(@Nullable Context context) {
         super(context, "Login.db", null, 5);
     }
 
-    /**
-     * creates the table user, so we can enter the login information of the user
-     * @param db
-     */
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE user(email TEXT PRIMARY KEY, password TEXT)");
+        db.execSQL("CREATE TABLE user(email TEXT PRIMARY KEY, password TEXT, roommate1 TEXT, roommate2 TEXT, roommate3 TEXT, roommate4 TEXT)");
     }
 
-    /**
-     * doesn't create a new table if the table already exists
-     * @param db
-     * @param oldVersion
-     * @param newVersion
-     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS user");
@@ -41,7 +29,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * a method that inserts the information into the database
      * @param email: the email the user signed up with
      * @param password: the password the user entered
-     * @return
      */
     public boolean insert (String email, String password){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -76,5 +63,51 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.update("user", contentValues, "email=?", new String[]{email});
         return true;
     }//end method checkCredentials
+
+    public boolean insert1 (String r1, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("roommate1", r1);
+        long ins = db.update("user", contentValues, "email=?", new String[]{email});
+        return ins != -1;
+    }
+
+    public boolean insert2 (String r1, String r2, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("roommate1", r1);
+        contentValues.put("roommate2", r2);
+        long ins = db.update("user", contentValues, "email=?", new String[]{email});
+        return ins != -1;
+    }
+
+    public boolean insert3 (String r1, String r2, String r3, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("roommate1", r1);
+        contentValues.put("roommate2", r2);
+        contentValues.put("roommate3", r3);
+        long ins = db.update("user", contentValues, "email=?", new String[]{email});
+        return ins != -1;
+    }
+
+    public boolean insert4 (String r1, String r2, String r3, String r4, String email) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("roommate1", r1);
+        contentValues.put("roommate2", r2);
+        contentValues.put("roommate3", r3);
+        contentValues.put("roommate4", r4);
+        long ins = db.update("user", contentValues, "email=?", new String[]{email});
+        return ins != -1;
+    }
+
+    public boolean update (String key, String email, String update){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(key, update);
+        long ins = db.update("user", contentValues, "email=?", new String[]{email});
+        return ins != -1;
+    }
 
 }
