32a92a31855b571a95b215baf3abdb8096f4ae82
Nicole Droi <nicoled0210@hotmail.com>
2020-05-06 18:11:26 -0400

    Commit all Welcome page elements

--- app/src/main/java/com/example/choremates/WelcomePage.java
+++ app/src/main/java/com/example/choremates/WelcomePage.java
@@ -1,14 +1,31 @@
 package com.example.choremates;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class WelcomePage extends AppCompatActivity {
-
+public class WelcomePage extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        Button nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        openCalendarPage();
+    }// end OnClick
+
+    private void openCalendarPage() {
+        Intent intent = new Intent(this, CalendarViewPage.class);
+        startActivity(intent);
+    }// end openCalendarPage methos
 }
